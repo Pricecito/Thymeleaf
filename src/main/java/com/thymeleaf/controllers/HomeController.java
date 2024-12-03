00f6475d4f14bb1040192008c57bc99a11966e81
Pricecito <matas.gutierrez.1254@gmail.com>
2024-12-03 17:23:41 -0500

    Agregando modelo

--- src/main/java/com/thymeleaf/controllers/HomeController.java
+++ src/main/java/com/thymeleaf/controllers/HomeController.java
@@ -1,15 +1,23 @@
 package com.thymeleaf.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+    /*
+     * @Value("${indice.hola}")
+     * private String hola; CON ESTEA ETIQUETA SE PUEDEN LEER VALOR DE
+     * APLICATION.PROPERTIES
+     */
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        String hola = "HOLA MUNDO CON THYMELEAF";
+        model.addAttribute("model1", hola);
         return "index";
     }
 
